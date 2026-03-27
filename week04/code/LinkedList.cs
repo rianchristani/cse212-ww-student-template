9ec9399b4898a6f295423be0d8c7530f23901932
Rian Christani <rianchristani@hotmail.com>
2026-03-27 20:55:22 -0300

    week 04

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Globalization;
 
 public class LinkedList : IEnumerable<int>
 {
@@ -32,7 +33,18 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void InsertTail(int value)
     {
-        // TODO Problem 1
+        Node newNode = new(value);
+
+        if (_head is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        } else
+        {
+            newNode.Prev = _tail;
+            _tail.Next = newNode;
+            _tail = newNode; 
+        }
     }
 
 
@@ -64,7 +76,16 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void RemoveTail()
     {
-        // TODO Problem 2
+        if (_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        } 
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null;
+            _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
@@ -108,7 +129,33 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void Remove(int value)
     {
-        // TODO Problem 3
+
+        if (_head == null) return;
+    
+        if (_head.Data == value)
+        {
+            RemoveHead();
+            return;
+        }
+        
+        var current = _head.Next;
+            while (current != null)
+            {
+                if (current.Data == value)
+                {
+                    if (current == _tail)
+                    {
+                      RemoveTail();  
+                    }
+                    else
+                    {
+                    current.Next!.Prev = current.Prev;
+                    current.Prev!.Next = current.Next;
+                    }
+                    return;
+                }
+                current = current.Next;
+            }
     }
 
     /// <summary>
@@ -116,7 +163,16 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void Replace(int oldValue, int newValue)
     {
-        // TODO Problem 4
+        var current = _head;
+
+        while (current != null)
+        {
+            if (current.Data == oldValue)
+            {
+                current.Data = newValue;
+            }
+            current = current.Next;
+        }
     }
 
     /// <summary>
